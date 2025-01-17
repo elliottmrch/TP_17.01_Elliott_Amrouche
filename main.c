5c147cb1f3dcb7b884e94ca2d9d110889aab14e5
Elliott Amrouche <elliottamrouche27@gmail.com>
2025-01-17 14:46:48 +0100

    Pointeurs en paramètre de fonction

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    int a = 2 ;
-    printf(" %p ", &a) ;
-    int *p = &a ;
-    printf(" %p " , p ) ;
-    printf(" %p " , &p ) ;
-    *p = 5 ;
-    printf(" %d " , a ) ;
-    return 0;
+void min_max(int tab[] , int size, int* min , int *max)
+{
+    if (size < 1)
+    {
+        return ;
+    }
+    *min = tab[0];
+    *max = tab[0];
+    for (int i = 0 ; i < size ; i++)
+    {
+        if(*min > tab[i])
+        {
+            *min = tab[i];
+        }
+        if(*max < tab[i])
+        {
+            *max = tab[i];
+        }
+    }
+}
+
+int main()
+{
+    int min = 0 ;
+    int max = 0;
+    int tableau[] = {12, 2 , 8 , -7 , 15 , 2} ;
+    min_max(tableau, 6, &min, &max) ;
+    printf(" min = %d, max = %d\n ", min, max) ;
+    return 0 ;
 }
