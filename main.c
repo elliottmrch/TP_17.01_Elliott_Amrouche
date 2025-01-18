d8f581230d3f3b20c82fe923bc2682c790c2100a
Elliott Amrouche <elliottamrouche27@gmail.com>
2025-01-18 21:05:01 +0100

    Etape 5 Rendu Final

--- main.c
+++ main.c
@@ -41,6 +41,27 @@ void runApplication(struct Application * app)
     }
 }
 
+void updateMenu(struct Application * app, int menuIndex, const char * newOption)
+{
+    if (menuIndex >= 0 && menuIndex < app->nb_menus)
+    {
+        app->menus[menuIndex].option = newOption;
+        printf("L'option %d a ete mise a jour avec succes.\n", menuIndex + 1);
+    }
+    else
+    {
+        printf("Index de menu invalide.\n");
+    }
+}
+
+void updateApplication(struct Application * app, const char * newName, struct Menu * newMenus, int newMenuCount)
+{
+    app->nom = newName;
+    app->menus = newMenus;
+    app->nb_menus = newMenuCount;
+    printf("L'application a ete mise a jour avec succes.\n");
+}
+
 void runPhone(struct Application * app1, struct Application * app2)
 {
     int choix = -1;
@@ -49,6 +70,8 @@ void runPhone(struct Application * app1, struct Application * app2)
         printf("--- Applications ---\n");
         printf("1. %s\n", app1->nom);
         printf("2. %s\n", app2->nom);
+        printf("3. Modifier le menu de '%s'\n", app1->nom);
+        printf("4. Modifier l'application '%s'\n", app2->nom);
         printf("0. Quitter\n");
         scanf_s("%d", &choix);
 
@@ -60,6 +83,36 @@ void runPhone(struct Application * app1, struct Application * app2)
             case 2:
                 runApplication(app2);
                 break;
+            case 3:
+                {
+                    int menuIndex;
+                    char newOption[100];
+                    printf("Entrez l'index du menu a modifier (1 a %d) : ", app1->nb_menus);
+                    scanf_s("%d", &menuIndex);
+                    printf("Entrez la nouvelle option : ");
+                    scanf_s("%99s", newOption, 100);
+                    updateMenu(app1, menuIndex - 1, newOption);
+                }
+                break;
+            case 4:
+                {
+                    char newName[100];
+                    struct Menu newMenus[10];
+                    int newMenuCount;
+                    printf("Entrez le nouveau nom de l'application : ");
+                    scanf_s("%99s", newName, 100);
+                    printf("Entrez le nombre de nouvelles options de menu : ");
+                    scanf_s("%d", &newMenuCount);
+                    for (int i = 0; i < newMenuCount; i++)
+                    {
+                        char newOption[10][100];
+                        printf("Entrez la nouvelle option %d : ", i + 1);
+                        scanf_s("%99s", newOption[i], 100);
+                        newMenus[i].option = newOption[i];
+                    }
+                    updateApplication(app2, newName, newMenus, newMenuCount);
+                }
+                break;
             case 0:
                 printf("Exit\n");
                 break;
@@ -69,7 +122,6 @@ void runPhone(struct Application * app1, struct Application * app2)
         }
     }
 }
-
 
 int main()
 {
@@ -99,8 +151,6 @@ int main()
         sizeof(MenusMessages) / sizeof(MenusMessages[0])
     };
 
-    //displayMenu(&Photos);
-    //runApplication(&Photos);
     runPhone(&Photos, &Messages);
 
     return 0 ;
